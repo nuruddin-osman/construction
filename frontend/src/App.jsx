96757b2e37eb85781c73a99bbe9cb9ff61c2bd64
nuruddin-osman <mdnuruddin378350@gmail.com>
2025-04-17 20:30:37 +0600

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const App = () => {
+  return (
+    <div className="btn-primary">
+      <button className="btn btn-primary">Hello</button>
+    </div>
+  );
+};
+
+export default App;
