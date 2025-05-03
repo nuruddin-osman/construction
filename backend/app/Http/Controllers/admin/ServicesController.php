86497f8066692d96c2c968848ad8375008802458
mnosumon <mdnuruddin378350@gmail.com>
2025-05-04 00:53:20 +0600

    create services api done

---
+++ backend/app/Http/Controllers/admin/ServicesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\services;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
+
+class ServicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validation = Validator::make($request->all(),[
+            'title'=>'required',
+            'slug'=>'required|unique:services,slug',
+        ]);
+        if ($validation->fails()) {
+            return response()->json([
+                'status'=>false,
+                'errors'=>$validation->errors()
+            ]);
+        }
+        $model = new Services();
+        $model->title = $request->title;
+        $model->slug = Str::slug($request->slug);
+        $model->short_desc = $request->short_desc;
+        $model->description = $request->description;
+        $model->status = $request->status;
+        $model->save();
+
+        return response()->json([
+            'status'=> true,
+            'message'=> 'success'
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(services $services)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(services $services)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, services $services)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(services $services)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\admin\DashboardController;
+use App\Http\Controllers\admin\ServicesController;
 use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 Route::post('/authenticate',[AuthenticationController::class, 'authenticate']);
@@ -11,6 +12,11 @@ Route::post('/authenticate',[AuthenticationController::class, 'authenticate']);
  
 
 Route::middleware('auth:sanctum')->group(function () {
+    // Protected route
     Route::get('/dashboard', [DashboardController::class, 'index']);
     Route::post('/logout', [AuthenticationController::class, 'logout']);
+
+    // Servicces route
+    Route::post('/services', [ServicesController::class, 'store']);
+
 });
