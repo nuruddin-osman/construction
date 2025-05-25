6ad3e4144d9e492346129e166b84bd5f76ad1376
mnosumon <mdnuruddin378350@gmail.com>
2025-05-25 20:38:33 +0600

    constact mail dynamic working

---
+++ backend/app/Http/Controllers/frontend/ContactController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\frontend;
+
+use App\Http\Controllers\Controller;
+use App\Mail\ContactEmail;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+
+class ContactController extends Controller
+{
+    public function index(Request $request){
+        $validation = Validator::make($request->all(),[
+            "name"=> "required",
+            "email"=> "required",
+        ]);
+        if ($validation->fails()) {
+           return response()->json([
+                "status"=> false,
+                "errors"=> $validation->errors()
+           ]);
+        }
+
+        $mailData = [
+            "name"=> $request->name,
+            "email"=> $request->email,
+            "phone"=> $request->phone,
+            "subject"=> $request->subject,
+            "message"=> $request->message,
+        ];
+
+        Mail::to("admin@example.com")->send(new ContactEmail($mailData));
+    }
+}
